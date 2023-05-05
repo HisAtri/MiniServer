558d266700886defd0d31b8eab5b339382911ac1
HisAtri <yz@ghacg.com>
2023-05-05 12:13:17 +0800

    logger

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import http.server
 import os
 import socketserver
 import http.client
+import logging
 
 from def_global import get_first_path
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+file_handler = logging.FileHandler('log.txt')
+file_handler.setLevel(logging.WARNING)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -26,5 +38,5 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
 PORT = 8080
 Handler = MyHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
+logger.info("serving at port"+str(PORT))
 httpd.serve_forever()
