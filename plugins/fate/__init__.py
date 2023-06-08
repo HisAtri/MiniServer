6cad38744c6bb92e6e2afacf1c5dbdf6c7fcb0ea
HisAtri <yz@ghacg.com>
2023-06-08 15:04:15 +0800

    补充御神签类型，修改字体

--- plugins/fate/__init__.py
+++ plugins/fate/__init__.py
@@ -12,10 +12,10 @@ rootdir = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(rootdir, "image")
 jpg_img = [filename for filename in os.listdir(image_dir) if filename.endswith('.jpg')]
 font_path = os.path.join(rootdir, "font/font.ttf")
-print (font_path)
+font_path2 = os.path.join(rootdir, "font/qkwt.ttf")
 
 def getext():
-    fatext = random.choice(["大\n\n吉","中\n\n吉","小\n\n吉","末\n\n吉",])
+    fatext = random.choice(["大\n\n吉","中\n\n吉","小\n\n吉","\n吉\n","末\n\n吉","\n凶\n","大\n\n凶"])
     fatile = random.choice([
         "昔我往矣\n杨柳依依\n今我来思\n雨雪霏霏",
         "有酒有酒\n闲饮东窗\n愿言怀人\n舟车靡从",
@@ -58,14 +58,13 @@ def getimg():
     font = ImageFont.truetype(font_path, size=84)
     text = "今日运势"
     img_draw = ImageDraw.Draw(img)
-    # 文字描边
     outline_color = (255, 255, 255)
     outline_width = 2
     bbox = img_draw.textbbox((0, 0), text, font=font)
     w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
     text_width, text_height = img_draw.textsize(text, font)
     x = (img.width - text_width) // 2
-    y = (img.height - text_height) // 8  # 垂直居中
+    y = (img.height - text_height) // 8
     for xo in range(-outline_width, outline_width + 1, 4):
         for yo in range(-outline_width, outline_width + 1, 4):
             img_draw.text((x + xo, y + yo), text, fill=outline_color, font=font)
@@ -73,24 +72,22 @@ def getimg():
 
     text = getext()
     font = ImageFont.truetype(font_path, size=108)
-    # 文字描边
     bbox = img_draw.textbbox((0, 0), text[0], font=font)
     w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
     text_width, text_height = img_draw.textsize(text[0], font)
     x = (img.width - text_width) // 2
-    y = (img.height - text_height) // 3  # 垂直居中
+    y = (img.height - text_height) // 3
     for xo in range(-outline_width, outline_width + 1, 4):
         for yo in range(-outline_width, outline_width + 1, 4):
             img_draw.text((x + xo, y + yo), text[0], fill=outline_color, font=font)
     img_draw.text((x, y), text[0], fill=(255, 0, 0), font=font)
 
-    font = ImageFont.truetype(font_path, size=54)
-    # 文字描边
+    font = ImageFont.truetype(font_path2, size=54)
     bbox = img_draw.textbbox((0, 0), text[1], font=font)
     w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
     text_width, text_height = img_draw.textsize(text[1], font)
     x = (img.width - text_width) // 2
-    y = (img.height - text_height) * 0.7  # 垂直居中
+    y = (img.height - text_height) * 0.7
     for xo in range(-outline_width, outline_width + 1, 2):
         for yo in range(-outline_width, outline_width + 1, 2):
             img_draw.text((x + xo, y + yo), text[1], fill=outline_color, font=font)
@@ -112,7 +109,7 @@ def handle_request(self):
     else:
         date = now.strftime("%Y%m%d")   #20230201格式
         seed = remove_first_path(self.path) + date
-    random.seed(seed)               #全局随机数种子
+    random.seed(seed)                   #全局随机数种子
 
     img = getimg()
 
